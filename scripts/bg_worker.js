4af8627d023579066ff00cf62b5c26232fb685e3
LazyKarlson <LazyKarlson@users.noreply.github.com>
2017-02-15 00:08:49 +0300

    Before prealpha

--- scripts/bg_worker.js
+++ scripts/bg_worker.js
@@ -12,7 +12,7 @@ var actions = {
 	updateInboxComments: function(request, callback) {        
         var refreshInterval = localStorage.getItem('refreshInterval');
 	chrome.cookies.getAll({url:"http://dirty.ru/"}, function(cookies) {	
-	receiveData(cookies[0].value, cookies[1].value);	   
+	getInboxes(cookies[0].value, cookies[1].value);	   
   		});
 	},
 
@@ -46,40 +46,41 @@ var actions = {
     }
 }
 
-var alertTypes = ['comment_answer',
-'mention',
-'new_comment',
-'post_became_gold',
-'permission_grant',
-'given_gold',
-'ban',
-'unban',
-'president_elected',
-'election_voting_start',
-'election_nomination_start',
-'post_from_subscribed_user'];
+var alertTypes = {"comment_answer":"Ответы",
+"mention":"Упоминания",
+"new_comment":"Новые комментарии",
+"post_became_gold":"Золотой пост",
+"permission_grant":"Получите лопату",
+"given_gold":"Получите золото",
+"ban":"Вас забанили",
+"unban":"Вас разбанили",
+"president_elected":"Выборы закончились",
+"election_voting_start":"Голосование началось",
+"election_nomination_start":"Выдвижение началось",
+"post_from_subscribed_user":"Новый пост"};
 
-function receiveData(u_id, s_id){
+function getInboxes(u_id, s_id){
 xhr.open('GET', "https://dirty.ru/api/inboxes/unread/", true);
 xhr.setRequestHeader('X-Futuware-UID', u_id);
 xhr.setRequestHeader('X-Futuware-SID', s_id);
-xhr.onreadystatechange = processRequest;
+xhr.onreadystatechange = processInboxes;
 xhr.send();
 }
 
 function markRead(u_id, s_id, i_id){
 localStorage.removeItem('inbox'+i_id);
-var inboxUrl = "https://dirty.ru/api/inbox/" + i_id + "/view/"    
+var inboxUrl = "https://dirty.ru/api/inbox/" + i_id + "/view/";    
 req.open('POST', inboxUrl, true);
 req.setRequestHeader('X-Futuware-UID', u_id);
 req.setRequestHeader('X-Futuware-SID', s_id);
-req.onreadystatechange = receiveData(u_id, s_id);
+req.onreadystatechange = getInboxes(u_id, s_id);
 req.send();
 }
 
 function getMyAlerts(u_id, s_id){
-var inboxUrl = "https://dirty.ru/api/my/notifications/"    
-subs.open('GET', inboxUrl, true);
+var alertsUrl = "https://dirty.ru/api/my/notifications/unread/"; 
+//var inboxUrl = "https://dirty.ru/api/my/notifications/mark_read/"; 
+subs.open('GET', alertsUrl, true);
 subs.setRequestHeader('X-Futuware-UID', u_id);
 subs.setRequestHeader('X-Futuware-SID', s_id);
 subs.onreadystatechange = processAlerts;
@@ -89,11 +90,44 @@ subs.send();
 function processAlerts(e) {
     if (subs.readyState == 4) {
         var response = JSON.parse(subs.responseText);
+        localStorage.setItem('totalAlertsCount', response["item_count"]);
+        if (response["item_count"] > 0){
         var alerts = response['notifications'];
-        alertTypes.forEach(function(item, index, array) {
-        var filtered = filterAlerts(alerts, item);
-        console.log(filtered); 
-});               
+        var jsonAlerts = "{";
+        for (var key in alertTypes) {
+        var filtered = filterAlerts(alerts, key);
+        if (filtered > 0){ 
+        jsonAlerts += '"' + key + '" :' + filtered + ', ';}       
+        }               
+        jsonAlerts = jsonAlerts.substr(0, jsonAlerts.length-2);
+        jsonAlerts += "}";
+        var alertsCount = JSON.parse(jsonAlerts);        
+        var prepareList = [];
+        if(typeof alertsCount != "undefined" &&  Object.keys(alertsCount).length > 0) {            
+            Object.keys(alertsCount).forEach(function(k) {
+                var new_alert_count = alertsCount[k] - localStorage.getItem('count_'+k);                
+                if(new_alert_count > 0) {
+                    prepareList.push({
+                                    title: alertTypes[k],
+                                    message: alertsCount[k] + "/" + new_alert_count
+                            });
+                 } 
+                 localStorage.setItem('count_'+k, alertsCount[k]);               
+            });
+                if (localStorage.getItem('showDesktopNotifications') === "true") {
+            var desktopNotificationTemplate = {
+                type: 'list',
+                title: "Новые уведомления, %username%!",
+                message: "",
+                iconUrl: 'images/128.png',
+                items: prepareList
+            }
+                chrome.notifications.create('alerts', desktopNotificationTemplate);
+            
+            }            
+        }
+        }
+        drawBadge();
     }
 }
 
@@ -102,15 +136,19 @@ function filterAlerts(alerts, type){
         return obj.type == type;
         });
     localStorage.setItem(type, JSON.stringify(result));
+    if (localStorage.getItem('count_'+type) == null){
+            localStorage.setItem('count_'+type, 0);            
+        }
     return result.length;       
 }
 
-function processRequest(e) {
+function processInboxes(e) {
     if (xhr.readyState == 4) {
     	var response = JSON.parse(xhr.responseText);    	
     	localStorage.setItem('inboxcomments', JSON.stringify(response.inboxes));
     	localStorage.setItem('lastUpdate', moment().format('X'));
     	var inboxes = response['inboxes'];
+        //удаляем пустые
         if (inboxes.length == 0){
                 Object.keys(localStorage)
                 .forEach(function(key){
@@ -119,6 +157,7 @@ function processRequest(e) {
             }
        });
             };
+        //считаем количество новых с последнего обновления и показываем    
 	    if(typeof inboxes != "undefined" &&  inboxes.length > 0) {
 	    	var iCount = inboxes.length;            
 	    	var mCount = 0;	    	   	
@@ -151,17 +190,30 @@ function processRequest(e) {
             audio.play();
         	}
         	lastInboxCommentsCount = mCount;
-	       chrome.browserAction.setBadgeBackgroundColor({ color: [255, 123, 85, 255] });
-	       chrome.browserAction.setBadgeText({text: iCount +'/'+ mCount });
-	       chrome.browserAction.setIcon({path: 'images/icon.png'});	                          
+	       //chrome.browserAction.setBadgeBackgroundColor({ color: [255, 123, 85, 255] });
+	       //chrome.browserAction.setBadgeText({text: iCount +'/'+ mCount });
+	       //chrome.browserAction.setIcon({path: 'images/icon.png'});	                          
 	    } else {
 
 	        localStorage.setItem('inboxCommentsCount', '0');
-	        chrome.browserAction.setBadgeText({text: ''});
-	        chrome.browserAction.setIcon({path: 'images/icongray.png'});
+	        //chrome.browserAction.setBadgeText({text: ''});
+	        //chrome.browserAction.setIcon({path: 'images/icongray.png'});
 	        lastInboxCommentsCount = 0;        
 	    }
+        drawBadge();
 	}	
+}
+
+function drawBadge(){
+    if (localStorage.getItem('totalAlertsCount') > 0 || localStorage.getItem('inboxCommentsCount')>0){
+        chrome.browserAction.setBadgeBackgroundColor({ color: [255, 123, 85, 255] });
+        chrome.browserAction.setBadgeText({text: localStorage.getItem('totalAlertsCount') +'/'+ localStorage.getItem('inboxCommentsCount') });
+        chrome.browserAction.setIcon({path: 'images/icon.png'});
+    } else {
+        chrome.browserAction.setBadgeText({text: ''});
+        chrome.browserAction.setIcon({path: 'images/icongray.png'});
+    }
+
 }
 
 actions.initNotificationsInterval(); 
